4b7f135562c51b6b940c013692c7cb99b5f5d9e4
Hashir Mahmood <hashirmahmood4270@gmail.com>
2023-09-12 23:08:27 +0500

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import AppContext from '@/Context/AppContext'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from 'react-hot-toast'
